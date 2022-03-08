8e88bea061bb9df1211f3ad9ed3309eee1ea1a50
SreeyaSrikanth <101204974+SreeyaSrikanth@users.noreply.github.com>
2022-03-08 22:37:50 +0530

    Added comments

--- Project/DbOperations.py
+++ Project/DbOperations.py
@@ -1,4 +1,6 @@
 import mysql.connector
+
+#Function to open a database connection for query execution
 def openDbConnection():
     try:
             dbConnection=mysql.connector.connect(host="localhost",user="root",password="0410",database="project")
@@ -6,6 +8,7 @@ def openDbConnection():
     except mysql.connector.Error as sqlerror:
             print("DB error {}".format(sqlerror))
 
+#Function to close the database connection after query execution
 def closeDbConnection(dbConnection):
     try:
             dbConnection.close()       
